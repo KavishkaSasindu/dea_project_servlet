63f106a038ffb0fb8e458e0bc5f40851ab2211e4
PemiduHerath <mprherath@students.nsbm.ac.lk>
2024-04-27 00:21:18 -0300

    implement addProduct method

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -1,5 +1,32 @@
 package dao;
 
-public class ProductDao {
+import db.DbConnection;
+import models.Product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProductDao {
+    Connection conn;
+
+    public ProductDao() {
+        DbConnection db = new DbConnection();
+        conn=db.getConnection();
+    }
+    public boolean addProduct(Product p) throws SQLException {
+        int insertRaw = 0;
+        String quary = "INSERT INTO product(product_name,product_price,description,main_image,image_one,image_two,image_three) VALUES(?,?,?,?,?,?,?)";
+        PreparedStatement ps = conn.prepareStatement(quary);
+        insertRaw = ps.executeUpdate();
+        ps.setString(1,p.getPname());
+        ps.setFloat(2,p.getPprice());
+        ps.setString(3,p.getDes());
+        ps.setString(4, p.getMimage());
+        ps.setString(5,p.getImg1());
+        ps.setString(6,p.getImg2());
+        ps.setString(7,p.getImg3());
+
+        return insertRaw<0;
+    }
 }
