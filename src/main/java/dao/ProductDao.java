da7cbe4f1caa53f99cacb88f7a7064d6414e9fc8
PemiduHerath <mprherath@students.nsbm.ac.lk>
2024-04-26 23:47:18 -0300

    implement product model

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -1,5 +1,5 @@
 package dao;
 
 public class ProductDao {
-    
+
 }
--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -1,5 +1,69 @@
 package models;
 
 public class Product {
+    private String pname;
+    private float pprice;
+    private String des;
+    private String mimage;
+    private String img1;
+    private String img2;
+    private String img3;
 
+    public Product(){}
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public float getPprice() {
+        return pprice;
+    }
+
+    public void setPprice(float pprice) {
+        this.pprice = pprice;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getMimage() {
+        return mimage;
+    }
+
+    public void setMimage(String mimage) {
+        this.mimage = mimage;
+    }
+
+    public String getImg1() {
+        return img1;
+    }
+
+    public void setImg1(String img1) {
+        this.img1 = img1;
+    }
+
+    public String getImg2() {
+        return img2;
+    }
+
+    public void setImg2(String img2) {
+        this.img2 = img2;
+    }
+
+    public String getImg3() {
+        return img3;
+    }
+
+    public void setImg3(String img3) {
+        this.img3 = img3;
+    }
 }
