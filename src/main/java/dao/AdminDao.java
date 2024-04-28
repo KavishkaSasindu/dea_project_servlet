1a9b298b0f450033f77b7e7933f9c6d402968760
dtdrupasinghe <thisara.damith@gmail.com>
2024-04-28 16:13:25 +0530

    impelement adminDao calss

---
+++ src/main/java/dao/AdminDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+import db.DbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AdminDao {
+
+    Connection conn;
+    public AdminDao(){
+        DbConnection dbConnection = new DbConnection();
+        conn = dbConnection.getConnection();
+    }
+
+    public boolean signInAdmin(String email,String password){
+        try{
+            final String ADMIN_QUERY = "SELECT * from admin WHERE email=? AND password=?";
+            PreparedStatement stmt = conn.prepareStatement(ADMIN_QUERY);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return true;
+            }else{
+                return false;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+}
