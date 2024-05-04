3f009c1fe5303f838498a950ffba8af88dbe08de
KavishkaSasindu <sasindu0215@gmail.com>
2024-05-04 21:53:49 +0530

    implement cart basic logic

--- src/main/java/dao/CartDAO.java
+++ src/main/java/dao/CartDAO.java
@@ -15,15 +15,29 @@ public class CartDAO {
         conn = dbConnection.getConnection();
     }
 
-    public boolean addCart(CartItem cartItem,int productId){
+    public boolean addCart(CartItem cartItem,int productId,String email){
+        int insertRow = 0;
         try{
-            final String INSERT = "INSERT INTO product(productid,pname,pprice,description,image1,image2,image3,image4,quantity,category) VALUES(?,?,?,?,?,?,?,?,?,?)";
+            final String INSERT = "INSERT INTO product(productid,pname,pprice,description,image1,image2,image3,image4,quantity,category,email) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement stmt = conn.prepareStatement(INSERT);
             stmt.setInt(1,productId);
+            stmt.setString(2,cartItem.getpName());
+            stmt.setString(3, cartItem.getpPrice());
+            stmt.setString(4,cartItem.getpDescription());
+            stmt.setString(5,cartItem.getMainImage());
+            stmt.setString(6,cartItem.getImg1());
+            stmt.setString(7,cartItem.getImg2());
+            stmt.setString(8,cartItem.getImg3());
+            stmt.setInt(9,cartItem.getQuantity());
+            stmt.setString(10,cartItem.getCategory());
+            stmt.setString(11,email);
+
+            insertRow = stmt.executeUpdate();
         }catch(Exception e){
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
+        return insertRow>0;
     }
 
     public CartItem CartOne(int productId){
--- src/main/java/web/CartServlet.java
+++ src/main/java/web/CartServlet.java
@@ -5,24 +5,47 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpSession;
 import models.AdminModel;
+import models.CartItem;
 import models.User;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 @WebServlet("/cartAdd")
 public class CartServlet extends HttpServlet{
 
-        public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException{
-            try{
-                int id = Integer.parseInt(request.getParameter("productId"));
+ public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException{
+     try{
 
-                CartDAO cartDAO = new CartDAO();
-            }catch(Exception e){
-                e.printStackTrace();
-                System.out.println(e.getMessage());
-            }
-        }
+         PrintWriter out = response.getWriter();
+
+         ArrayList<CartItem> cartList = new ArrayList<>();
+
+         int productId = Integer.parseInt(request.getParameter("productId"));
+
+         CartItem cartItem = new CartItem();
+         cartItem.setId(productId);
+         cartItem.setQuantity(2);
+
+
+         HttpSession session = request.getSession();
+         ArrayList<CartItem> cart_list = (ArrayList<CartItem>) session.getAttribute("cart-list");
+
+         if(cart_list == null){
+             cartList.add(cartItem);
+             session.setAttribute("cart_list",cartList);
+             out.println("session created and added to cart list");
+         }
+
+
+     }catch (Exception e){
+         e.printStackTrace();
+         System.out.println(e.getMessage());
+     }
+ }
 
 
 }
